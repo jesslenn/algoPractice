d2a96cd2659793b768269b901db6cd21e6f1e844
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2021-11-12 13:20:27 -0500

    first commit.  really.

---
+++ algoThought/arrayThreeSum.js
@@ -0,0 +1,16 @@
+// Interviewer Prompt
+// Given an array of distinct integers and an integer representing a target sum, write a function that returns an array of all triplets in the input array that sum to the target sum.
+
+// Examples
+// arrayThreeSum([12, 3, 1, 2, -6, 5, -8, 6], 0)   //should return [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+// arrayThreeSum([5, 6 , 1, -9 , 7, 3 , 2], 35)    //should return []
+// arrayThreeSum([1, 15, -5, 12 , -3, 6 , 2], 10)  //should return [[ -3, 1, 12 ]]
+//sort the array!
+//initialize a for loop
+//we will have three pointers-- [i], [i+1], [arr.length-1]
+
+
+
+// arrayThreeSum([12, 3, 1, 2, -6, 5, -8, 6], 0)
+// arrayThreeSum([5, 6 , 1, -9 , 7, 3 , 2], 35)
+// arrayThreeSum([1, 15, -5, 12 , -3, 6 , 2], 10)
---
+++ algoThought/pairSum.js
@@ -0,0 +1,15 @@
+// Interviewer Prompt
+// Given an array arr consisting of N integers, sorted in ascending order (least to greatest), and a separate number (a sum), determine if any 2 numbers in the array add up to the sum. Return true if any 2 different numbers within the array add up to sum. Return false if no 2 numbers in the array add up to sum.
+
+// Examples
+// pairSum([1, 1, 2, 3, 4, 5], 7) -> true (either 2+5 or 3+4)
+// pairSum([1, 2, 3, 4, 5], 10) -> false
+// pairSum([0, 2, 3, 6, 9, 10], 10) -> true (0 + 10)
+// pairSum([1, 2, 3, 7, 8], 7) -> false
+// pairSum([-2, 0, 4, 6, 10], 8) -> true (-2 + 10)
+// pairSum([1, 2, 3, 4, 5], 2) -> false
+// Examples - Edge cases
+
+// pairSum([1], 2) -> false
+// pairSum([2], 2) -> false
+// pairSum([], 1) -> false
