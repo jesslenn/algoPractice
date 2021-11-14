1ef2fed39c3a6c8c847316398ed6634f45771d96
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2021-11-14 18:42:37 -0500

    DOC: added comments into arrayThreeSum

--- algoThought/arrayThreeSum.js
+++ algoThought/arrayThreeSum.js
@@ -8,10 +8,48 @@
 //sort the array!
 //initialize a for loop
 //we will have three pointers-- [i], [i+1], [arr.length-1]
+//POINTERS:
+function arrayThreeSum (arr, targetSum) {
+  //sorts our array from least to most
+  arr.sort((a, b) => a-b)
+  //initialize our return array
+  let solution = [];
+  //We're going to loop through the array--if we know we'll be initializing a pointer at the end of the array, then we know we don't need to check the element at length-1 because our end pointer will catch it.  So i only has to go to length-2
+  for (let i = 0; i < arr.length-2; i++) {
+    //first pointer
+    let element = arr[i];
+    //second pointer
+    let leftIndex = i + 1;
+    //third pointer
+    //element    leftIndex                 rightIndex
+    //[1,        3,         5, 6, 8, 11,    97]
+    let rightIndex = arr.length-1;
+  }
+}
 
-//hello world
+//MEMO:
 function arrayThreeSum (array, sum) {
-
+  //initializes an empty array in which we can return our solution
+  const solution = [];
+  //we set up an initial loop to find our first value
+  for (let i = 0; i < array.length-1; i++){
+    //Okay. If we already have arr[i]; what's our new target sum?
+    //aka the sum we're going to have given we already know one element
+    let currentSum = sum - array[i]
+    //initialize our memo!
+    let memo = {};
+    //same as pairSum--we never want array[i] === array[j] so we initialize it one index after i
+    for (let j = i + 1; j < array.length; j++) {
+      //if our memo ALREADY CONTAINS the remainder that we need to get our sum
+      if (memo[currentSum-array[j]]){
+        //we push integers at our locations in each loop, AND the remainder integer that we found in our memo
+        solution.push(array[i], currentSum-array[j], array[j])
+      } else {
+        //if we don't find our remainder in the memo, we'll add the value at j into our memo for use later in our loop(s)!
+        memo[array[j] = true]
+      }
+    }
+  }
 }
 
 arrayThreeSum([12, 3, 1, 2, -6, 5, -8, 6], 0)
