b34750d53ea076b1c00a2c841b5bb9c84006ec55
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-15 11:41:23 -0500

    Add: shiftLL in Python

--- stacks-queues-linkedLists/shiftLL.js
+++ stacks-queues-linkedLists/shiftLL.js
@@ -1,4 +1,4 @@
-//Prompt: Given a singly linked list, rotate the linked list counter-clockwise by k nodes. Where k is a given positive integer. For example, if the given linked list is 10->20->30->40->50->60 and k is 4, the list should be modified to 50->60->10->20->30->40. Each node has an integer value as well as a next value point to the next node in the list or to null if it's at the end of the list.  You can assyme the LL will always have at least one node--the head will never be null.
+
 
 // This is the class of the input linked list.
 class LinkedList {
@@ -12,6 +12,7 @@ function shiftLinkedList(head, k) {
   
 }
 
+shiftLinkedList()
 //Steps to get to the mutated linked list:
 //0 > 1 > 2 > 3 > 4 > 5
 //4 > 5 > 0 > 1 > 2 > 3
@@ -19,3 +20,10 @@ function shiftLinkedList(head, k) {
 //New head needs to be reassigned
 //New tail needs to be reassigned to null
 //We only care about four nodes--the old head and tail and the new head and tail.
+//We can only get the original tail in the question by traversing the entire list, so our lower 
+//bound for time is O(n)
+//If we can find the new tail, it will already be pointed to the new head -- so those operations
+//will be tied together
+//We can find the length of the list and the original tail in the same operation
+//new tail is LLLength - k
+
