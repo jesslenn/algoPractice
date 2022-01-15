549dd451806a4c005464b936ba1d8fa7f391750d
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-15 11:28:09 -0500

    Added shiftLL

---
+++ stacks-queues-linkedLists/shiftLL.js
@@ -0,0 +1,17 @@
+//Prompt: Given a singly linked list, rotate the linked list counter-clockwise by k nodes. Where k is a given positive integer. For example, if the given linked list is 10->20->30->40->50->60 and k is 4, the list should be modified to 50->60->10->20->30->40. Each node has an integer value as well as a next value point to the next node in the list or to null if it's at the end of the list.  You can assyme the LL will always have at least one node--the head will never be null.
+
+// This is the class of the input linked list.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function shiftLinkedList(head, k) {
+  // Write your code here.
+}
+
+// Do not edit the lines below.
+exports.LinkedList = LinkedList;
+exports.shiftLinkedList = shiftLinkedList;
