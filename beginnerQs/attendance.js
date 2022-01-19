a67f0617e86bb36bb39bb95312a5580a1c68d7f8
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-19 14:42:40 -0500

    Add: explanation for attendance.js 1

--- beginnerQs/attendance.js
+++ beginnerQs/attendance.js
@@ -43,3 +43,13 @@ let classRoom = [
 ];
 
 //[{name: [{day: bool}, {day: bool}, {day: bool}]}, {[{}, {}, {}]}, {[{}, {}, {}]}]
+//Big, complicated array of objects, right?  So while we're looping through, we reassign EVERYTHING so we know what we're handling at each step.
+
+function attendanceCheck(day){
+  let presentStudents = []
+  //we are looping through each student--so every i comes back with an object containing an array of objects
+  for (let i = 0; i < classRoom.length; i++){
+    //individual student object
+    let eachStudent = classRoom[i]
+  }
+}
