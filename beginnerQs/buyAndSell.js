a2f22b8d9ae0877d25256d0d3129bd538811c9a0
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-24 15:32:25 -0500

    Add: annotating buyAndSell.js

--- beginnerQs/buyAndSell.js
+++ beginnerQs/buyAndSell.js
@@ -16,14 +16,22 @@ Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
+//we know our function is going to take our prices as an array
 function mostProfit(pricesArr) {
+  //One of the things that is a pretty frequent pattern when finding the most or least of something is the use of variables to store them and the idea that if you want to store the most, you'll initialize the variable at the least.
+  //Here we see that in action: mostProfit starts at 0 because we don't have any info yet and we want to be able to go up.  min we start at Infinity because we want to make sure that any possible variable we store here later will be able to go down.
   let mostProfit = 0;
   let min = Infinity;
+  //Now we loop through our array of prices
   for (let i = 0; i < pricesArr.length; i++) {
+    //first we check that value and see if it's less than our min--on our first pass, it is guaranteed to be less than the initial value of min.
     if (pricesArr[i] < min){
+      //if it is less, we store the new value in min
       min = pricesArr[i]
     }
+    //we now check to see what the difference is between our current value and our min to find the current profit
     let profit = pricesArr[i] - min;
+    //if this profit is greater than what we've found for mostProfit so far (remember: on our first pass this is very likely going to be the case unless we get a 0 or negative number)
     if (profit > mostProfit) {
       mostProfit = profit
     }
