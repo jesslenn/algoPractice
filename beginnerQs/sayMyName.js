18540ea3cfbf5679b89a4ededda25489115ba77b
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2022-01-24 12:25:27 -0500

    Add: say your name

--- beginnerQs/sayMyName.js
+++ beginnerQs/sayMyName.js
@@ -8,3 +8,13 @@ let me = {
 }
 
 me.getGreeting();
+
+//Say Your Name
+//Create a new object, me2 that contains a name property and a getGreeting methoc--this time, the method should accept an object that contains a name property as an argument.  The returned greeting should now greet the other object by name
+
+let me2 = {
+  name: 'Grace',
+  getGreeting: function(otherName) {
+    return 'Hi ' + otherName.name + ', my name is ' + this.name + '.'
+  }
+}
